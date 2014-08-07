d301a88432808457f48d7bf37db0f64d9f6d51c0
Corey Rowe <CoreyR1196@gmail.com>
2014-08-06 22:55:11 -0400

    Added imports and variable declarations Added jsoup library Created radioGroup

--- app/src/main/java/com/GBSnowDay/SnowDay/snow_day.java
+++ app/src/main/java/com/GBSnowDay/SnowDay/snow_day.java
@@ -1,5 +1,6 @@
 package com.GBSnowDay.SnowDay;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,16 +10,91 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.IOException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import com.GBSnowDay.SnowDay.R;
 
+public class snow_day extends Activity {
+    //Variable declaration
+    public String orgName;
+    public String status;
+    public String hazardName;
+    public String schooltext;
+    public String weathertext;
+    public String weathercheck;
+    public String today;
+    public String tomorrow;
+    public Date date;
+    public Format formatter;
 
-public class snow_day extends ActionBarActivity {
+    public String[] orgNameLine;
+    public String[] statusLine;
+
+    public int days;
+    public int schoolpercent = 0;
+    public int weatherpercent = 0;
+    public int percent;
+    public int dayrun = 0;
+    public int tier1 = 0;
+    public int tier2 = 0;
+    public int tier3 = 0;
+    public int tier4 = 0;
+    public int tier5 = 0;
+
+    public boolean schoolNull;
+    public boolean GBAcademy;
+    public boolean HolyFamily;
+    public boolean WPAcademy;
+    public boolean GISD;
+    public boolean Durand; //Check for "Durand Senior Center"
+    public boolean Holly;  //Check for "Holly Academy"
+    public boolean Lapeer; //Check for "Chatfield School-Lapeer", "Greater Lapeer Transit Authority", "Lapeer CMH Day Programs",
+    //"Lapeer Co. Ed-Tech Center", "Lapeer County Ofices", "Lapeer District Library", "Lapeer Senior Center", and "St. Paul Lutheran-Lapeer"
+    public boolean Owosso; //Check for "Owosso Senior Center", "Baker College-Owosso", and "St. Paul Catholic-Owosso"
+    public boolean Beecher;
+    public boolean Clio; //Check for "Clio Area Senior Center", "Clio City Hall", and "Cornerstone Clio"
+    public boolean Davison; //Check for "Davison Senior Center", "Faith Baptist School-Davison", and "Montessori Academy-Davison"
+    public boolean Fenton; //Check for "Lake Fenton", "Fenton City Hall", and "Fenton Montessori Academy"
+    public boolean Flushing; //Check for "Flushing Senior Citizens Center" and "St. Robert-Flushing"
+    public boolean Genesee; //Check for "Freedom Work-Genesee Co.", "Genesee Christian-Burton", "Genesee Co. Mobile Meals", "Genesee Hlth Sys Day Programs", "Genesee Stem Academy", and "Genesee I.S.D."
+    public boolean Kearsley;
+    public boolean LKFenton;
+    public boolean Linden; //Check for "Linden Charter Academy"
+    public boolean Montrose; //Check for "Montrose Senior Center"
+    public boolean Morris;  //Check for "Mt Morris Twp Administration" and "St. Mary's-Mt. Morris"
+    public boolean SzCreek; //Check for "Swartz Creek Area Senior Ctr." and "Swartz Creek Montessori"
+    public boolean Atherton;
+    public boolean Bendle;
+    public boolean Bentley;
+    public boolean Flint; //Thankfully this is listed as "Flint Community Schools" - otherwise there would be 25 exceptions to check for.
+    public boolean Goodrich;
+    public boolean Carman; //Check for "Carman-Ainsworth Senior Ctr."
+    public boolean GB; //Check for "Freedom Work-Grand Blanc", "Grand Blanc Academy", "Grand Blanc City Offices", "Grand Blanc Senior Center", and "Holy Family-Grand Blanc"
+
+    //Figure out what tomorrow is
+    //Saturday = 0, Sunday = 1
+    Calendar calendar = Calendar.getInstance();
+    int weekday = calendar.get(Calendar.DAY_OF_WEEK);
+    int month = calendar.get(Calendar.MONTH);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snow_day);
+
+
     }
 
 
@@ -45,4 +121,6 @@ public class snow_day extends ActionBarActivity {
         Intent i = new Intent(getApplicationContext(), snow_day_result.class);
         startActivity(i);
     }
+
+
 }
