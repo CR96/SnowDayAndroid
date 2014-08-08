d28cca8c72c026ee72e9c62a7014fd6d528140bc
Corey Rowe <CoreyR1196@gmail.com>
2014-08-08 12:19:29 -0400

    Implemented static radar image from web

--- app/src/main/java/com/GBSnowDay/SnowDay/snow_day_result.java
+++ app/src/main/java/com/GBSnowDay/SnowDay/snow_day_result.java
@@ -1,20 +1,94 @@
 package com.GBSnowDay.SnowDay;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
 import com.GBSnowDay.SnowDay.R;
 
-public class snow_day_result extends ActionBarActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class snow_day_result extends Activity {
+
+    public static final String weatherURL = "http://radar.weather.gov/Conus/Loop/centgrtlakes_loop.gif";
+            ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snow_day_result);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //Create an object for subclass of AsyncTask
+        GetXMLTask task = new GetXMLTask();
+        //Execute the task
+        task.execute(new String[]{weatherURL});
     }
 
+    private class GetXMLTask extends AsyncTask <String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap map = null;
+            for (String url : urls) {
+                map = downloadImage(url);
+            }
+            return map;
+        }
 
+        //Sets the Bitmap returned by doInBackground
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+
+        //Creates Bitmap from InputStream and returns it
+        private Bitmap downloadImage(String url) {
+            Bitmap bitmap = null;
+            InputStream stream = null;
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inSampleSize = 1;
+
+            try {
+                stream = getHttpConnection(url);
+                bitmap = BitmapFactory.decodeStream(stream, null, bmOptions);
+                stream.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        //Makes HttpUrlConnection and returns InputStream
+        private InputStream getHttpConnection(String urlString) throws IOException {
+            InputStream stream = null;
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+
+            try {
+                HttpURLConnection httpConnection = (HttpURLConnection) connection;
+                httpConnection.setRequestMethod("GET");
+                httpConnection.connect();
+
+                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    stream = httpConnection.getInputStream();
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return stream;
+        }
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
