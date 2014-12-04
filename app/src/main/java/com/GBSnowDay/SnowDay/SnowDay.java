db0187b2ec2ef8d97adf818cf65ca8400c884886
Corey Rowe <CoreyR1196@gmail.com>
2014-12-04 08:10:07 -0500

    Added Apache 2.0 license Create landcape variant of About activity

--- app/src/main/java/com/GBSnowDay/SnowDay/SnowDay.java
+++ app/src/main/java/com/GBSnowDay/SnowDay/SnowDay.java
@@ -28,28 +28,19 @@ import java.util.Date;
 import java.util.List;
 
 public class SnowDay extends Activity {
+    /*Copyright 2014 Corey Rowe
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.snow_day, menu);
-        return true;
-    }
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_about) {
-            //Show the about activity
-            Intent about = new Intent(getApplicationContext(), About.class);
-            startActivity(about);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.*/
 
     //Declare all views
     RadioButton optToday;
@@ -165,6 +156,28 @@ public class SnowDay extends Activity {
                 startActivity(result);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.snow_day, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            //Show the about activity
+            Intent about = new Intent(getApplicationContext(), About.class);
+            startActivity(about);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //Adapter class
